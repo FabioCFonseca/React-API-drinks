e634ab935c719794b24231db20b1ed04c0e45043
Fabio <fc_102@hotmail.com>
2022-12-15 14:09:39 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import SingleCocktail from './pages/SingleCocktail';
-import Error from './pages/Error';
 // import components
 import Navbar from './components/Navbar';
 function App() {
@@ -15,7 +14,6 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='cocktail/:id' element={<SingleCocktail />} />
-        {/*<Route path='error' element={<Error />} />*/}
       </Routes>
     </Router>
   );
